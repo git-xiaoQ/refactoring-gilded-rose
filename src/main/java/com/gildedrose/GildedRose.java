fbeafd0761e6c947314e230f2f6864521b7b5a85
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-03-01 17:46:28 +0800

    change if to switch

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,53 +9,49 @@ class GildedRose {
 
     public void update_quality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sell_in < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sell_in < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sell_in = items[i].sell_in - 1;
-            }
-
-            if (items[i].sell_in < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+            switch (items[i].name){
+                case "Aged Brie":
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
-                }
+                    items[i].sell_in = items[i].sell_in - 1;
+                    if (items[i].sell_in < 0) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+
+                case "Backstage passes to a TAFKAL80ETC concert"  :
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+
+                        if (items[i].sell_in < 11) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+
+                        if (items[i].sell_in < 6) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+
+                    }
+                    items[i].sell_in = items[i].sell_in - 1;
+                    if (items[i].sell_in < 0) {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+
+                case "Sulfuras, Hand of Ragnaros":
+                    continue;
+
+                default:
+                    if (items[i].quality > 0) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                    items[i].sell_in = items[i].sell_in - 1;
+                    if (items[i].sell_in < 0) {
+                        if (items[i].quality > 0) {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
             }
         }
     }
