160facc9d8887df7d31f9dae10209a518a4abcfe
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-03-01 19:20:52 +0800

    extracted method of update_item_sell_in in GildedRose class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,17 +11,24 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             switch (items[i].name){
                 case "Aged Brie":
+                    items[i].sell_in = update_item_sell_in(items[i].sell_in);
+
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
-                    items[i].sell_in = items[i].sell_in - 1;
+
                     if (items[i].sell_in < 0) {
                         if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 2;
+                        }else {
                             items[i].quality = items[i].quality + 1;
+
                         }
                     }
 
                 case "Backstage passes to a TAFKAL80ETC concert"  :
+                    items[i].sell_in = update_item_sell_in(items[i].sell_in);
+
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
 
@@ -32,9 +39,8 @@ class GildedRose {
                         if (items[i].sell_in < 6) {
                             items[i].quality = items[i].quality + 1;
                         }
-
                     }
-                    items[i].sell_in = items[i].sell_in - 1;
+
                     if (items[i].sell_in < 0) {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
@@ -43,10 +49,12 @@ class GildedRose {
                     continue;
 
                 default:
+                    items[i].sell_in = update_item_sell_in(items[i].sell_in);
+
                     if (items[i].quality > 0) {
                         items[i].quality = items[i].quality - 1;
                     }
-                    items[i].sell_in = items[i].sell_in - 1;
+
                     if (items[i].sell_in < 0) {
                         if (items[i].quality > 0) {
                             items[i].quality = items[i].quality - 1;
@@ -54,5 +62,10 @@ class GildedRose {
                     }
             }
         }
+    }
+
+    int update_item_sell_in(int sell_in){
+        sell_in -= 1;
+        return sell_in;
     }
 }
