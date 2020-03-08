ba255fe4c85d0c3b3389f264dc2f63090f7582f0
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-03-08 15:57:36 +0800

    add a safe net of gildedRose

---
+++ src/test/java/com/gildedrose/GildedRoseSafetyNet.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import com.google.common.io.Files;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.google.common.base.Charsets.UTF_8;
+import static org.junit.Assert.assertEquals;
+
+public class GildedRoseSafetyNet {
+    @Test
+    public void should_get_a_string_which_equal_with_baseline() throws IOException {
+        String output = TextTestFixture.getBaseLine();
+        String baseLine = Files.toString(new File("src/test/baseLine.txt"),UTF_8);
+        assertEquals(baseLine,output);
+    }
+
+}
